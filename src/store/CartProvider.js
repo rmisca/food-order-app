ba1026d0b500006db5399351306512e35c52623a
rmisca <rominamisca95@gmail.com>
2021-09-24 19:10:16 +0300

    Created a form component, Added validation on form inputs, Added data to Firebase

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -56,6 +56,10 @@ import CartContext from "./cart-context";
          };
         }
 
+        if (action.type === 'CLEAR') {
+            return defaultCartState;
+        }
+
      return defaultCartState;
  };
 
@@ -73,11 +77,16 @@ import CartContext from "./cart-context";
          dispatchCartAction({type: 'REMOVE', id: id});
      };
 
+     const clearCartHandler = () => {
+         dispatchCartAction({type: 'CLEAR'});
+     };
+
      const cartContext = {
          items: cartState.items,
          totalAmount: cartState.totalAmount,
          addItem: addItemToCartHandler,
          removeItem: removeItemFromCartHandler,
+         clearCart: clearCartHandler
      };
 
      return (
